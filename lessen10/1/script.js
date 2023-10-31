d5e8a6224cf0e9568419c1a7274cdf566442f2b4
Tr1nt1 <104519127+Tr1nt1@users.noreply.github.com>
2023-10-31 20:04:57 +0300

    Update script.js

--- lessen10/1/script.js
+++ lessen10/1/script.js
@@ -4,18 +4,16 @@ const popupContent = document.querySelector('.popup_content');
 const popupClose = document.querySelector('.popup_close');
 
 popupBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    popupBtn.classList.add("opened");
+    popup.classList.add("opened");
 })
 
 popupClose.addEventListener('click', (event) => {
-    event.preventDefault();
-    popupBtn.classList.remove("opened");
+    popup.classList.remove("opened");
 })
 
 window.addEventListener('click', (event) => {
-    event.preventDefault();
-    event.target();
-    popupBtn.classList.remove("opened");
+    if (event.target === popup) {
+        popup.classList.remove("opened");
+    }
 });
 
